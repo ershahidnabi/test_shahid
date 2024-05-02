01f449f895eb27ebe301a63187c0add2286ba395
ershahidnabi <ershahidnabi@gmail.com>
2024-05-02 11:31:37 +0530

    second commit at 11:30am

--- lib/myhome.dart
+++ lib/myhome.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -28,29 +29,164 @@ class _MyHomeState extends State<MyHome> {
           )
         ],
       ),
-      body: SafeArea(
+      body: const SafeArea(
           child: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                // decoration: BoxDecoration(),
-                child: Image.asset(
-                  'assets/code.jpg',
+            MyHeader(),
+            MyInfoWidget(),
+            MyLang(),
+            BlogWidget(),
+            Textbox(),
+            Social(),
+            Report(),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class MyHeader extends StatelessWidget {
+  const MyHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  'https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=600',
                   fit: BoxFit.cover,
                 ),
               ),
             ),
-            const MyInfoWidget(),
-            const MyLang(),
-            const BlogWidget(),
-            const Textbox(),
-            const Social(),
-            const Report(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(6)),
+                      child: const Row(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.locationPin,
+                            color: Colors.orange,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            "4 km away",
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: const FaIcon(FontAwesomeIcons.heart,
+                          color: Colors.orange),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          RichText(
+                              text: const TextSpan(
+                                  text: "Eliza Will ms",
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w200,
+                                      color: Colors.white),
+                                  children: [
+                                TextSpan(
+                                  text: "24",
+                                ),
+                              ])),
+                          const SizedBox(width: 5),
+                          const FaIcon(
+                            FontAwesomeIcons.angleDoubleDown,
+                            size: 22,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black38,
+                      ),
+                      child: Row(
+                        children: [
+                          const FaIcon(
+                            FontAwesomeIcons.locationPin,
+                            size: 18,
+                            color: Colors.orange,
+                          ),
+                          const SizedBox(width: 5),
+                          RichText(
+                              text: const TextSpan(
+                                  text: "Chenni,",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w200,
+                                      color: Colors.white),
+                                  children: [
+                                TextSpan(
+                                  text: "India",
+                                ),
+                              ])),
+                          const SizedBox(width: 5),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+                right: 10,
+                bottom: -20,
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50)),
+                  child: const FaIcon(FontAwesomeIcons.bookBookmark,
+                      color: Colors.orange),
+                ))
           ],
         ),
-      )),
+      ),
     );
   }
 }
